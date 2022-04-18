19efa4863cc0788615445d1415bc72392e17c82d
Git_TJ <98149148+GeoffreyGichaga@users.noreply.github.com>
2022-04-18 06:08:01 +0300

    not complete

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+
+
+function superbowlWin(record)
+{
+    let findWinners = record.find((item)=>{
+        if(item.result === "W")
+        {
+            return item["year"]
+        }
+    })
+    return findWinners.year
+
+}
+
+
+
+
+
